1a22376656d8ce01106f992cd6a55c1813c649b9
CodeAhmed <mansooronly4u@gmail.com>
2016-06-25 04:04:24 +0530

    Program to get the sum of its digits

--- CodePracticeCSharp/Program.cs
+++ CodePracticeCSharp/Program.cs
@@ -10,6 +10,18 @@ namespace CodePracticeCSharp
     {
         static void Main(string[] args)
         {
+            int number, sum = 0, digit, numbercopy;
+            Console.WriteLine("Enter number to get the Sum of its Digits :");
+            number = int.Parse(Console.ReadLine());
+            numbercopy = number;
+            while (number != 0)
+            {
+                digit = number % 10;
+                number = number / 10;
+                sum = sum + digit;
+            }
+            Console.WriteLine("The Sum of Digits of the Number {0}: ", sum);
+            Console.ReadLine();
         }
     }
 }
