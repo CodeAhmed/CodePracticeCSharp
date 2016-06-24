95589508d845530072d5b89bf89bbec222a987eb
CodeAhmed <mansooronly4u@gmail.com>
2016-06-25 01:41:43 +0530

    Program to swpa two numbers

--- CodePracticeCSharp/Program.cs
+++ CodePracticeCSharp/Program.cs
@@ -10,6 +10,27 @@ namespace CodePracticeCSharp
     {
         static void Main(string[] args)
         {
+            int first, second, temp = 0;
+            string choice="Y";
+            while(choice=="Y" || choice=="y")
+            {
+                Console.WriteLine("Enter First Number : ");
+                first = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number : ");
+                second = int.Parse(Console.ReadLine());
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Before Swapping");
+                Console.WriteLine("First Number - " + first +": Second Number - " + second);
+                temp = first;
+                first = second;
+                second = temp;
+                Console.WriteLine("After Swapping");
+                Console.WriteLine("First Number - " + first + ": Second Number - " + second);
+                Console.WriteLine("Would you like to Continue..... Y or N");
+                choice = Console.ReadLine();
+                
+            }
+            
         }
     }
 }
