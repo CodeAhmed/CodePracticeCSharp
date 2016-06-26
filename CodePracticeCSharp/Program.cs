284f2b741e6b52448098ee1ffe88c2a27ad1f07f
CodeAhmed <mansooronly4u@gmail.com>
2016-06-27 02:16:45 +0530

    Program to get the reverse of any digits

--- CodePracticeCSharp/Program.cs
+++ CodePracticeCSharp/Program.cs
@@ -10,6 +10,19 @@ namespace CodePracticeCSharp
     {
         static void Main(string[] args)
         {
+            int number, reverse = 0;
+            Console.WriteLine("Enter any Number to find the reverse");
+            number = int.Parse(Console.ReadLine());
+            while (number != 0)
+            {
+                reverse = reverse * 10;
+                reverse = ((number % 10) + reverse);
+                number = number / 10;
+                
+            }
+            Console.WriteLine("The reverse of the digits of the number is " + reverse);
+
+
         }
     }
 }
